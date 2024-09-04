fbf538335f87c2b3fc504db804a209d8c561fe74
Albina Peposhi <codegirl.al@gmail.com>
2024-09-04 17:06:50 +0200

    Actual fix - hopefully 2x

--- srcs/requirements/tools/make_dir.sh
+++ srcs/requirements/tools/make_dir.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/data
 mkdir -p ~/data/mariadb
 mkdir -p ~/data/wordpress
-sudo chmod -R 755 /home/flafi/data/wordpress
-sudo chmod -R 777 /home/flafi/data/mariadb
+sudo chmod -R 755 /home/apeposhi/data/wordpress
+sudo chmod -R 777 /home/apeposhi/data/mariadb
 
 echo deeznuts
